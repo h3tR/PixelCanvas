0341415384dfb7fc42f90c8d6f1bef4a934bf338
Markh3tr <155853551+Markh3tr@users.noreply.github.com>
2025-01-06 21:41:57 +0100

    Basic brorken 3D projection & rendering

--- src/main/java/ml/jozefpeeterslaan72wuustwezel/graphics/Renderer3D.java
+++ src/main/java/ml/jozefpeeterslaan72wuustwezel/graphics/Renderer3D.java
@@ -1,12 +1,11 @@
 package ml.jozefpeeterslaan72wuustwezel.graphics;
 
 import org.joml.Vector2f;
-import org.joml.Vector2i;
 import org.joml.Vector3f;
 
 public class Renderer3D {
-    Vector3f camPos;
-    Vector3f camRot;
+    public Vector3f camPos;
+    public Vector3f camRot;
     Vector3f DisplaySurfaceRelativePosition = new Vector3f(0, 0, 1);
 
     public Renderer3D(Vector3f camPos, Vector3f camRot) {
@@ -15,15 +14,18 @@ public class Renderer3D {
     }
 
     public Vector2f project3DPoint(Vector3f point){
+        Vector3f affectedPoint = new Vector3f(point).sub(camPos);
         Vector3f camCoordDistance = new Vector3f(
-                (float) (Math.cos(camRot.y)*(Math.sin(camRot.z)*point.y+Math.cos(camRot.z)*point.x)-Math.sin(camRot.y)*point.z),
-                (float) (Math.sin(camRot.x)*(Math.cos(camRot.y)*point.z+Math.sin(camRot.y)*(Math.sin(camRot.z)*point.y+Math.cos(camRot.x)*point.x))+Math.cos(camRot.x)*(Math.cos(camRot.z)*point.y-Math.sin(camRot.z)* point.x)),
-                (float) (Math.cos(camRot.x)*(Math.cos(camRot.y)*point.z+Math.sin(camRot.y)*(Math.sin(camRot.z)*point.y+Math.cos(camRot.x)*point.x))-Math.sin(camRot.x)*(Math.cos(camRot.z)*point.y-Math.sin(camRot.z)* point.x))
+                (float) (Math.cos(camRot.y)*(Math.sin(camRot.z)*affectedPoint.y+Math.cos(camRot.z)*affectedPoint.x)-Math.sin(camRot.y)*affectedPoint.z),
+                (float) (Math.sin(camRot.x)*(Math.cos(camRot.y)*affectedPoint.z+Math.sin(camRot.y)*(Math.sin(camRot.z)*affectedPoint.y+Math.cos(camRot.x)*affectedPoint.x))+Math.cos(camRot.x)*(Math.cos(camRot.z)*affectedPoint.y-Math.sin(camRot.z)* affectedPoint.x)),
+                (float) (Math.cos(camRot.x)*(Math.cos(camRot.y)*affectedPoint.z+Math.sin(camRot.y)*(Math.sin(camRot.z)*affectedPoint.y+Math.cos(camRot.x)*affectedPoint.x))-Math.sin(camRot.x)*(Math.cos(camRot.z)*affectedPoint.y-Math.sin(camRot.z)* affectedPoint.x))
         );
         return new Vector2f(
                 DisplaySurfaceRelativePosition.z/camCoordDistance.z*camCoordDistance.x+DisplaySurfaceRelativePosition.x,
                 DisplaySurfaceRelativePosition.z/camCoordDistance.z*camCoordDistance.y+DisplaySurfaceRelativePosition.y
-
                 );
+    }
+    public void setFOV(double fov) {
+        DisplaySurfaceRelativePosition.z = (float) Math.tan(fov);
     }
 }
--- src/main/java/ml/jozefpeeterslaan72wuustwezel/jade/Window.java
+++ src/main/java/ml/jozefpeeterslaan72wuustwezel/jade/Window.java
@@ -1,6 +1,7 @@
 package ml.jozefpeeterslaan72wuustwezel.jade;
 
 import ml.jozefpeeterslaan72wuustwezel.graphics.FramePlotter2D;
+import ml.jozefpeeterslaan72wuustwezel.graphics.Renderer3D;
 import org.joml.*;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFW;
@@ -9,6 +10,8 @@ import org.lwjgl.opengl.GL;
 import org.lwjgl.system.MemoryUtil;
 
 import java.lang.Math;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.lwjgl.opengl.GL11.*;
 
@@ -87,22 +90,60 @@ public class Window {
         float FrameEnded = FrameStarted;
         float deltaTime = -1.0f;
         Random rand = new Random();
-        FramePlotter2D plotter2D = new FramePlotter2D(Dimensions);
+        Renderer3D renderer3D = new Renderer3D(new Vector3f(0),new Vector3f(0));
 
-       float i = 0;
-       float factor = .2f;
+        float camSpeed = .02f;
+        Map<Integer,Vector3f> MovementVectors = new HashMap<>();
+        MovementVectors.put(90,new Vector3f(0,0,1));//Z
+        MovementVectors.put(81,new Vector3f(-1,0,0));//Q
+        MovementVectors.put(83,new Vector3f(0,0,-1));//S
+        MovementVectors.put(68,new Vector3f(1,0,0));//D
+        MovementVectors.put(69,new Vector3f(0,-1,0));//E
+        MovementVectors.put(67,new Vector3f(0,1,0));//C
+        Map<Integer,Vector3f> RotationVectors = new HashMap<>();
+        RotationVectors.put(264,new Vector3f(-1,0,0));//UP
+        RotationVectors.put(265,new Vector3f(1,0,0));//DOWN
+        RotationVectors.put(263,new Vector3f(0,-1,0));//LEFT
+        RotationVectors.put(262,new Vector3f(0,1,0));//RIGHT
+
+
+
+        float i = 0;
+        float factor = .2f;
         while(!GLFW.glfwWindowShouldClose(glfwWindow)){
             //Poll Events
             GLFW.glfwPollEvents();
             //glClear(GL_COLOR_BUFFER_BIT);
+            FramePlotter2D plotter2D = new FramePlotter2D(Dimensions);
+            Vector3f[] cubeVertices = new Vector3f[]{
+                    new Vector3f(-1,-1,-1),new Vector3f(-1,-1,1),new Vector3f(-1,1,-1),new Vector3f(-1,1,1),
+                    new Vector3f(1,-1,-1),new Vector3f(1,-1,1), new Vector3f(1,1,-1),new Vector3f(1,1,1)
+            };
+            int[][] polygons = new int[][]{
+                    {1,5,7},{1,7,3},//back
+                    {0,1,3},{0,3,2},//left
+                    {4,0,3},{4,3,2},//front
+                    {5,4,6},{5,6,7},//right
+                    {3,7,6},{3,6,2},//top
+                    {0,3,5},{0,5,1}//bottom
+            };
+            for(int[] polygon : polygons){
+                Vector2f p0 = renderer3D.project3DPoint(new Vector3f(cubeVertices[polygon[0]]).add(0,0,10));
+                Vector2f p1 = renderer3D.project3DPoint(new Vector3f(cubeVertices[polygon[1]]).add(0,0,10));
+                Vector2f p2 = renderer3D.project3DPoint(new Vector3f(cubeVertices[polygon[2]]).add(0,0,10));
+                plotter2D.plotTriangle(
+                        new Vector2i((int) (p0.x*Dimensions.x), (int) (p0.y*Dimensions.y)),
+                        new Vector2i((int) (p1.x*Dimensions.x), (int) (p1.y*Dimensions.y)),
+                        new Vector2i((int) (p2.x*Dimensions.x), (int) (p2.y*Dimensions.y)),
+                        new Vector3f(1,1,1)
+                );
+            }
 
-
-            plotter2D.fillTriangle(new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector3f(1,0,0));
-            plotter2D.fillTriangle(new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector3f(0,1,0));
-            plotter2D.fillTriangle(new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector3f(0,0,1));
+            //plotter2D.fillTriangle(new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector3f(1,0,0));
+           // plotter2D.fillTriangle(new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector3f(0,1,0));
+          //  plotter2D.fillTriangle(new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector2i(rand.nextInt(Dimensions.x)-Dimensions.x/2,rand.nextInt(Dimensions.y)-Dimensions.y/2),new Vector3f(0,0,1));
 
             plotter2D.draw();
-
 
             GLFW.glfwSwapBuffers(glfwWindow);
 
@@ -111,8 +152,16 @@ public class Window {
             FrameStarted = FrameEnded;
             if(KeyListener.keyDown(32)) //space
                 System.out.println(1f/deltaTime+" FPS");
+            for(Integer keyCode: MovementVectors.keySet())
+                if(KeyListener.keyDown(keyCode))
+                   renderer3D.camPos.add(new Vector3f(MovementVectors.get(keyCode)).mul(camSpeed));
+
+            for(Integer keyCode: RotationVectors.keySet())
+                if(KeyListener.keyDown(keyCode))
+                    renderer3D.camRot.add(new Vector3f(RotationVectors.get(keyCode)).mul(camSpeed));
+
             i+=factor;
-            if(Math.abs(i)>5)
+            if(Math.abs(i)>10)
                 factor*=-1;
         }
     }
